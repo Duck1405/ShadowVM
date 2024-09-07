0e8607d493b5171dc9857a7468ece6fa01e5a92a
Duck1405 <rd105291@student.musd.org>
2024-09-06 23:46:15 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,18 @@
+#!/bin/bash
+
+sleep 2
 export HOME=/home/container
-
 cd /home/container
-
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
+# Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-curl -Ls https://raw.githubusercontent.com/Duck1405/ShadowVM/main/install.sh -o install.sh
+# Copy run.sh to /home/container
+cp /run.sh "$HOME/run.sh"
 
-chmod +x ./install.sh
+# Make run.sh executable.
+chmod +x "$HOME/run.sh"
 
-sh ./install.sh
+# Run the VPS Installer
+bash /install.sh
